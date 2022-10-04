3d6f0b0cc0bef136ad77d94a40dd5fba5c35382e
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-10-04 15:17:07 -0400

    Hooked API up to Search Function and Confirmed working

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import { options } from "./keys.js";
 
-const weatherApiUrl = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=';
+const weatherApiUrl = "https://weatherapi-com.p.rapidapi.com/forecast.json?q=";
 
-const searchForm = document.getElementById('weather-search');
+const searchForm = document.getElementById("weather-search");
 
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let requestedCity = e.target.cityinput.value;
-    //console.log(requestedCity); //Works
-    fetchCityWeather(requestedCity);
-    searchForm.reset();
-})
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let requestedCity = e.target.cityinput.value;
+  //console.log(requestedCity); //Works
+  fetchCityWeather(requestedCity);
+  searchForm.reset();
+});
 
 function fetchCityWeather(city) {
-    //console.log(city); //Works
-    fetch(`${weatherApiUrl}${city}&days=3`, options)
+  //console.log(city); //Works
+  fetch(`${weatherApiUrl}${city}&days=3`, options)
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
