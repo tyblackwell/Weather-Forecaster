fcc71695f2195a132ef9fc8709f4528e97ba0283
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-10-04 12:13:19 -0400

    Abstracted out the keys for the Forecast API - Fetch is working

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import { options } from "./keys.js";
+
+fetch(
+  "https://weatherapi-com.p.rapidapi.com/forecast.json?q=London&days=3",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
 const toggleBtn = document.querySelector(".toggle");
 
 toggleBtn.addEventListener("click", () => toggleBtn.classList.toggle("active"));
